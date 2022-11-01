c5a65ffe5ea590c90681f90b2b49c393428562ba
mrakesh0608 <mrakesh0608@outlook.com>
2022-11-01 23:48:03 +0530

    V2.1

--- src/helpers/Path.js
+++ src/helpers/Path.js
@@ -1,4 +1,4 @@
-// const url = 'https://personal-dict.herokuapp.com';
-const url = 'http://localhost:8000';
+const url = 'https://personal-dict.herokuapp.com';
+// const url = 'http://localhost:8000';
 
 export {url};
